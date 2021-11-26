be5f83d1c4df3f8637514ccb16d77f169d31161b
Enviie <50255493+Enviie@users.noreply.github.com>
2021-11-25 22:25:39 -0600

    Update Kiriot ESP Lib.lua

--- Kiriot ESP Lib.lua
+++ Kiriot ESP Lib.lua
@@ -2,6 +2,7 @@
 local ESP = {
     Enabled = false,
     Boxes = true,
+    Distance = true,
     BoxShift = CFrame.new(0,-1.5,0),
     BoxSize = Vector3.new(4,6,0),
     Color = Color3.fromRGB(255, 170, 0),
@@ -238,6 +239,22 @@ function boxBase:Update()
         end
     else
         self.Components.Name.Visible = false
+        self.Components.Distance.Visible = false
+    end
+
+    if ESP.Distance then
+        local TagPos, Vis5 = WorldToViewportPoint(cam, locs.TagPos.p)
+        
+        if Vis5 then
+            
+            self.Components.Distance.Visible = true
+            self.Components.Distance.Position = Vector2.new(TagPos.X, TagPos.Y + 14)
+            self.Components.Distance.Text = math.floor((cam.CFrame.p - cf.p).magnitude) .."m away"
+            self.Components.Distance.Color = color
+        else
+            self.Components.Distance.Visible = false
+        end
+    else
         self.Components.Distance.Visible = false
     end
     
