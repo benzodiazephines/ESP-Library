7773ea4ac6d54a889b0ad8d86221e285f5e02023
Enviie <50255493+Enviie@users.noreply.github.com>
2022-07-24 16:54:45 -0500

    Update Kiriot ESP Lib.lua

--- Kiriot ESP Lib.lua
+++ Kiriot ESP Lib.lua
@@ -158,7 +158,7 @@ boxBase.__index = boxBase
 function boxBase:Remove()
 	ESP.Objects[self.Object] = nil
 	for i,v in pairs(self.Components) do
-        if v.Name == "Highlight" then
+        if v == "Highlight" then
             v.Enabled = false
             v:Remove()    
         else
